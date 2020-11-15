de9c37c211d7121e8449e16fcc4d03afc2b6ab33
vkk316 <nuttanai03970@gmail.com>
2020-11-15 16:45:19 +0700

    reduce functions

---
+++ build/Employee.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Employee = void 0;
+var Employee = /** @class */ (function () {
+    function Employee(name, salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    return Employee;
+}());
+exports.Employee = Employee;
--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Employee_1 = require("./Employee");
+var employees = [
+    new Employee_1.Employee('Simon', 65000),
+    new Employee_1.Employee('Kumu', 45689),
+    new Employee_1.Employee('Nawawee', 200000)
+];
+var total_salary = employees.reduce(function (total, current) { return total + current.salary; }, 0);
+console.log("= " + total_salary);
 console.log("Hello world");
